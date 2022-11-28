983be4836e8600e1186f317784a34a87f9f70632
Sarah Thompson <sscotthompson@gmail.com>
2022-11-28 14:50:02 -0500

    simple rock, paper, scissors game

--- index.js
+++ index.js
@@ -64,12 +64,83 @@ if(earlyRegister && age > 18 ) {
   console.log(`Competitor ${raceNumber} will need to see the front desk!`)
 } 
 
-const plantNeedsWater = function(day){
-    if (day === 'Wednesday'){
-    return true;}
-    else {
-      return false;
+
+
+
+//////rock, paper, scissors game with
+
+
+//console.log('rock');
+
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'|| userInput === 'bomb') {
+      return userInput;
+    } else {
+      console.log('Error!')
     }
-  }
-  
-  console.log(plantNeedsWater('Tuesday'))
+    };
+    
+    const getComputerChoice = () => {
+      const randomNumber = Math.floor(Math.random() * 3);
+      switch (randomNumber) {
+      case 0:
+        return 'rock';
+        break;
+      case 1:
+        return 'paper';
+        break;
+      case 2:
+      return 'scissors';
+      break;
+    }
+    };
+    
+    const determineWinner =  (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+      return 'Game is a tie!';
+    }
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    
+    if (userChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    
+    if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    
+    if (userChoice === 'bomb') {
+      return 'Congratulations, you won!'
+    }
+    };
+    
+    //console.log(determineWinner('rock', 'scissors'));
+    //console.log(determineWinner('rock', 'rock'));
+    //console.log(determineWinner('paper', 'scissors'));
+    
+    const playGame = () => {
+      const userChoice = getUserChoice('paper');
+      const computerChoice = getComputerChoice();
+      console.log('You threw: ' + userChoice);
+      console.log('The computer threw:' + computerChoice);
+      console.log(determineWinner(userChoice, computerChoice));
+    };
+    
+    (playGame());
+    
